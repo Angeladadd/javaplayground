88a05aaffb8f4844cdd28b56741b95f42f63a163
Angeladaddy <sjtusunchenge@gmail.com>
2025-07-01 14:56:35 +0100

    webserver demo

---
+++ playground/src/main/java/com/angeladaddy/springplayground/playground/Greeting.java
@@ -0,0 +1,5 @@
+package com.angeladaddy.springplayground.playground;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ playground/src/test/java/com/angeladaddy/springplayground/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angeladaddy.springplayground.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
